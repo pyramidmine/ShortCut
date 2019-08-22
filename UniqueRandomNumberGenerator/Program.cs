63ee2a174ee814db6cac0ae9c09bad99ec81f68f
MoonHwan Lee <pyramidmine@gmail.com>
2019-08-22 09:56:52 +0900

    Add project files.

---
+++ UniqueRandomNumberGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueRandomNumberGenerator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Generate unique random numbers
+			List<int> numbers = Enumerable.Range(0, 1_000_000).OrderBy(x => Guid.NewGuid()).ToList();
+
+			// Check duplication
+			HashSet<int> uniqueNumbers = new HashSet<int>();
+			foreach (var n in numbers)
+			{
+				if (uniqueNumbers.Contains(n))
+				{
+					Console.WriteLine($"{n} is duplicated.");
+					break;
+				}
+				else
+				{
+					uniqueNumbers.Add(n);
+				}
+			}
+			Console.WriteLine("All numbers are unique.");
+		}
+	}
+}
