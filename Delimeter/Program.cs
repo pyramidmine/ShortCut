73ecdbaae31e2fe1d3969154b75532966a2f7a0c
pyramidmine <pyramidmine@gmail.com>
2019-09-18 15:53:40 +0900

    - 구분자 테스트 프로젝트 추가 - 생성자와 프로퍼티 초기화 테스트 프로젝트 추가

---
+++ Delimeter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delimeter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char verticalBar = (char)0x007C;
+			char verticalBrokenBar = (char)0x00A6;
+
+			StringBuilder sb = new StringBuilder();
+			sb.Append(verticalBar);
+			sb.Append(",");
+			sb.Append(verticalBrokenBar);
+			Console.WriteLine(sb.ToString());
+
+			byte[] encodedVerticalBar = Encoding.UTF8.GetBytes(new char[] { verticalBar });
+			byte[] encodedVerticalBrokenBar = Encoding.UTF8.GetBytes(new char[] { verticalBrokenBar });
+
+			Console.WriteLine($"{verticalBar}, {encodedVerticalBar.Length}");
+			Console.WriteLine($"{verticalBrokenBar}, {encodedVerticalBrokenBar.Length}");
+		}
+	}
+}
---
+++ PropertyInitializationOnConstructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyInitializationOnConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PropertyTest pt = new PropertyTest() { Name = "MoonHwan Lee" };
+            Console.WriteLine(pt.Name);
+        }
+    }
+
+    class PropertyTest
+    {
+        public string Name { get; set; }
+
+        public PropertyTest()
+        {
+        }
+    }
+}
