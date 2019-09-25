4e79dbb574d9e9e586fa79bb468711b82b3859f5
pyramidmine <pyramidmine@gmail.com>
2019-09-25 10:41:02 +0900

    - Move Logic class to Logic.cs file.

---
+++ GenericCollections/Logic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollections
+{
+	class Base { }
+
+	class Derived : Base { }
+
+	class Logic<T> where T : Base
+	{
+		List<T> items = new List<T>();
+
+		public void Add(T item)
+		{
+			items.Add(item);
+		}
+
+		public void PrintAll()
+		{
+			foreach (var v in items)
+			{
+				Console.WriteLine(v.ToString());
+			}
+		}
+	}
+}
--- GenericCollections/Program.cs
+++ GenericCollections/Program.cs
@@ -3,28 +3,6 @@ using System.Collections.Generic;
 
 namespace GenericCollections
 {
-	class Base { }
-
-	class Derived : Base { }
-
-	class Logic<T> where T : Base
-	{
-		List<T> items = new List<T>();
-
-		public void Add(T item)
-		{
-			items.Add(item);
-		}
-
-		public void PrintAll()
-		{
-			foreach (var v in items)
-			{
-				Console.WriteLine(v.ToString());
-			}
-		}
-	}
-
 	class Program
     {
         static void Main(string[] args)
