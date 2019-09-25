a8e62b310755ad7c8797fc4a3d41282659e06549
pyramidmine <pyramidmine@gmail.com>
2019-09-25 10:16:52 +0900

    - Generic type restriction test.

---
+++ GenericCollections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCollections
+{
+	class Base { }
+
+	class Derived : Base { }
+
+	class Logic<T> where T : Base
+	{
+		List<T> items = new List<T>();
+
+		public void Add(T item)
+		{
+			items.Add(item);
+		}
+
+		public void PrintAll()
+		{
+			foreach (var v in items)
+			{
+				Console.WriteLine(v.ToString());
+			}
+		}
+	}
+
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			Logic<Base> logic = new Logic<Base>();
+			logic.Add(new Base());
+			logic.Add(new Derived());
+			logic.PrintAll();
+        }
+    }
+}
