049396b8ead74138f6a534b09db893ad8b32afbd
pyramidmine <pyramidmine@gmail.com>
2019-09-25 10:37:40 +0900

    - Add IEnumerable class

---
+++ GenericCollections/Animals.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericCollections
+{
+	class Animal
+	{
+		public string Name { get; set; }
+	}
+
+	class Zoo<T> : IEnumerable<T>
+	{
+		List<T> items = new List<T>();
+
+		public void Add(T item)
+		{
+			items.Add(item);
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return items.GetEnumerator();
+		}
+
+		IEnumerator<T> IEnumerable<T>.GetEnumerator()
+		{
+			return items.GetEnumerator();
+		}
+	}
+}
--- GenericCollections/Program.cs
+++ GenericCollections/Program.cs
@@ -33,6 +33,14 @@ namespace GenericCollections
 			logic.Add(new Base());
 			logic.Add(new Derived());
 			logic.PrintAll();
+
+			Zoo<Animal> zoo = new Zoo<Animal>();
+			zoo.Add(new Animal() { Name = "Tiger" });
+			zoo.Add(new Animal() { Name = "Lion" });
+			foreach (var v in zoo)
+			{
+				Console.WriteLine(v.Name);
+			}
         }
     }
 }
