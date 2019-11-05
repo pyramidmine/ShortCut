26b009d1d7829377c043f684b305c639c6232eef
edward <edward.o2osys@gmail.com>
2019-11-05 22:38:03 +0900

    ExpressionSample, MultiThreadTest, QueryDeployer 프로젝트 추가

---
+++ MultiThreadTest/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MultiThreadTest
+{
+    class Program
+    {
+        static int TotalThreads { get; } = 5;
+        static int totalCount = 0;
+
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(DoSomething));
+            t.Start();
+            Console.WriteLine($"t.Background = {t.IsBackground}");
+            Thread.Sleep(30);
+            t.Abort();
+            t.Join();
+            Console.WriteLine($"Main: t.Join");
+
+            PrintThreadState(ThreadState.Aborted);
+            PrintThreadState(ThreadState.Aborted | ThreadState.AbortRequested);
+
+            Thread t2 = new Thread(new ThreadStart(DoSpin));
+            t2.Start();
+            Thread.Sleep(10);
+            Console.WriteLine($"Main: t2.Start executed");
+            t2.Interrupt();
+            t2.Join();
+            Console.WriteLine($"Main: t2.Join");
+
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < TotalThreads; i++)
+            {
+				threads.Add(new Thread(DoAdd));
+            }
+            foreach (var thread in threads)
+            {
+                thread.Start((object)80000);
+            }
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+            Console.WriteLine($"Program.totalCount = {totalCount}");
+        }
+
+        static void DoSomething()
+        {
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"DoSomething: {i}");
+                    Thread.Sleep(10);
+                }
+            }
+            catch (ThreadAbortException ex)
+            {
+                Console.WriteLine(ex);
+                Thread.ResetAbort();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Thread.ResetAbort();
+            }
+
+            Console.WriteLine($"End of DoSomething.");
+        }
+
+        static void PrintThreadState(ThreadState ts)
+        {
+            Console.WriteLine($"{ts,-16}: {(int)ts, -4}");
+        }
+
+        static void DoSpin()
+        {
+            try
+            {
+                Thread.SpinWait(2000000000);
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"DoSpin: {i}");
+                    Thread.Sleep(10);
+                }
+            }
+            catch (ThreadInterruptedException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.WriteLine($"End of DoSpin");
+        }
+
+        static void DoAdd(object count)
+        {
+			int loop = (int)count;
+            for (int i = 0; i < loop; i++)
+            {
+                Program.totalCount = Program.totalCount + 1;
+            }
+        }
+    }
+}
---
+++ QueryDeployer/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace QueryDeployer
+{
+	class Program
+	{
+		static string ServerIp { get; } = "172.18.99.2";
+		static short ServerPort { get; } = 1521;
+		static string DatabaseName { get; } = "xe";
+		static string UserId { get; } = "SYSTEM";
+		static string UserPassword { get; } = "Ekswnr59";
+
+		static int Main(string[] args)
+		{
+			if (args.Length < 2)
+			{
+				Console.WriteLine(Properties.Resources.Usage);
+				return -1;
+			}
+			string connectionString = $"Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST={ServerIp})(PORT={ServerPort})))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME={DatabaseName})));User ID={UserId};Password={UserPassword};Connection Timeout=30;Pooling=true;Statement Cache Size=1;";
+			string queryPackage =
+				@"CREATE OR REPLACE EDITIONABLE PACKAGE ""PKG_ORDER_LIST"" AS
+					PROCEDURE INSERT_ORDER (
+						in_ORDER_ID IN NUMBER,
+						in_ORDER_DATE IN DATE,
+						in_ORDER_PRICE IN NUMBER );
+					PROCEDURE SELECT_ORDER (
+						in_ORDER_DATE IN DATE );
+				END ""PKG_ORDER_LIST"";";
+			string queryPackageBody =
+				@"CREATE OR REPLACE EDITIONABLE PACKAGE BODY ""PKG_ORDER_LIST"" AS
+				/*
+				** Procedure: INSERT_ORDER
+				*/
+				PROCEDURE INSERT_ORDER (
+					in_ORDER_ID IN NUMBER,
+					in_ORDER_DATE IN DATE,
+					in_ORDER_PRICE IN NUMBER )
+				IS
+				BEGIN
+					INSERT INTO ORDER_LIST (ORDER_ID, ORDER_DATE, ORDER_PRICE)
+						VALUES(in_ORDER_ID, in_ORDER_DATE, in_ORDER_PRICE);
+				END INSERT_ORDER;
+				/*
+				** Procedure: SELECT_ORDER
+				*/
+				PROCEDURE SELECT_ORDER (
+					in_ORDER_DATE IN DATE )
+				IS
+				BEGIN
+					RETURN;
+				END SELECT_ORDER;
+				END ""PKG_ORDER_LIST"";";
+
+			try
+			{
+				using (OracleConnection conn = new OracleConnection(connectionString))
+				{
+					// Oracle PACKAGE command
+					using (OracleCommand cmd = new OracleCommand(queryPackage, conn))
+					{
+						conn.Open();
+						cmd.CommandType = CommandType.Text;
+						cmd.ExecuteNonQuery();
+
+						conn.Close();
+					}
+
+					// Oracle PACKAGE BODY command
+					using (OracleCommand cmd = new OracleCommand(queryPackageBody, conn))
+					{
+						conn.Open();
+						cmd.CommandType = CommandType.Text;
+						cmd.ExecuteNonQuery();
+						conn.Close();
+					}
+				}
+			}
+			catch (OracleException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			return 0;
+		}
+	}
+}
