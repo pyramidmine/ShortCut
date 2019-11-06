a1d72ec5a493065702d36560b1eeebab57fe6541
edward <edward.o2osys@gmail.com>
2019-11-06 10:00:21 +0900

    Task 관련 클래스 테스트

---
+++ TaskTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var task = Task.Run(() =>
+			{
+				Thread.Sleep(1000);
+				Console.WriteLine($"Task.Run");
+			});
+
+			Console.WriteLine("Main: After Task.Run");
+			task.Wait();
+		}
+	}
+}
