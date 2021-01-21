111f240d9df73b6f24405c5aca25d2cb425456a4
pyramidmine <pyramidmine@gmail.com>
2021-01-21 15:41:24 +0900

    Configuration 테스트 항목 추가
    - 컨피규레이션 변경 시에 이벤트 받는 지 테스트
    - 존재하지 않는 컨피규레이션 파일을 런타임에 로드할 수 있는 지 테스트

--- ConfigurationUnitTests/ConfigurationTest.cs
+++ ConfigurationUnitTests/ConfigurationTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
 using System;
 using System.IO;
 using System.Text.Json;
@@ -252,6 +253,52 @@ namespace ConfigurationUnitTests
 		}
 
 		/// <summary>
+		/// 컨피규레이션이 변경됐을 때 메시지를 받을 수 있는지 확인
+		/// </summary>
+		[Fact]
+		public void TestConfigurationChangedEvent()
+		{
+			const string configFilename = "testConfigurationChangedEvent.json";
+
+			var builder = new ConfigurationBuilder()
+				.AddJsonFile(configFilename, true, true)
+				.AddEnvironmentVariables();
+			var config = builder.Build();
+
+			// 컨피규레이션 변경 이벤트를 받았는지 여부
+			bool configChanged = false;
+
+			// 설정파일이 변경됐다는 이벤트를 받을 콜백 설정
+			ChangeToken.OnChange<IConfiguration>(
+				() => config.GetReloadToken(),
+				ConfigurationChangedCallback,
+				config);
+
+			// 컨피규레이션 변경
+			using (var fs = new FileStream(configFilename, FileMode.Create))
+			{
+				using (var writer = new Utf8JsonWriter(fs, new JsonWriterOptions { Indented = true }))
+				{
+					writer.WriteStartObject();
+					writer.WriteString("HOMEPATH", "MyHomePath");
+					writer.WriteEndObject();
+				}
+				fs.Flush();
+			}
+
+			// 컨피규레이션 변경 후 일정시간이 지나야 감지되므로 잠시 대기
+			Thread.Sleep(500);
+
+			Assert.True(configChanged);
+
+			void ConfigurationChangedCallback(IConfiguration configuration)
+			{
+				System.Diagnostics.Debug.WriteLine($"TestConfigurationChangedEvent.ConfigurationChangedCallback called");
+				configChanged = true;
+			}
+		}
+
+		/// <summary>
 		/// 런타임에 컨피규레이션 추가
 		/// </summary>
 		/// <remarks>
@@ -263,6 +310,76 @@ namespace ConfigurationUnitTests
 		[Fact]
 		public void TestAddConfigurationFileOnRuntime()
 		{
+			const string mainConfigFilename = "mainSettings.json";
+			const string subConfigFilename = "subSettings.json";
+
+			// 서브 컨피규레이션 파일은 테스트 전에 미리 삭제
+			if (File.Exists(subConfigFilename))
+			{
+				File.Delete(subConfigFilename);
+			}
+
+			// 메인 컨피규레이션 설정
+			// 일단, subSettingsOne.json 파일만 리스트에 있지만 실제로 파일은 없음
+			using (var fs = new FileStream(mainConfigFilename, FileMode.Create))
+			{
+				using (var writer = new Utf8JsonWriter(fs, new JsonWriterOptions { Indented = true }))
+				{
+					writer.WriteStartObject();
+					writer.WriteStartArray("SubConfigFiles");
+					writer.WriteEndArray();
+					writer.WriteEndObject();
+				}
+				fs.Flush();
+			}
+
+			var builder = new ConfigurationBuilder()
+				.AddJsonFile(mainConfigFilename);
+			var config = builder.Build();
+
+			// 현재 설정 상태 확인
+			Assert.Null(config.GetSection("SubConfigFiles")?.Get<string[]>());
+			Assert.Null(config["Packet,1,1,1:enabled"]);
+
+			// 서브 컨피규레이션 파일을 만들고 설정항목을 넣기
+			using (var fs = new FileStream(subConfigFilename, FileMode.Create))
+			{
+				using (var writer = new Utf8JsonWriter(fs, new JsonWriterOptions { Indented = true }))
+				{
+					writer.WriteStartObject();
+					writer.WriteStartObject("Packet,1,1,1");
+					writer.WriteString("enabled", "true");
+					writer.WriteEndObject();
+					writer.WriteEndObject();
+				}
+				fs.Flush();
+			}
+
+			// 메인 컨피규레이션 파일에 서브 컨피규레이션 파일 추가
+			using (var fs = new FileStream(mainConfigFilename, FileMode.Create))
+			{
+				using (var writer = new Utf8JsonWriter(fs, new JsonWriterOptions { Indented = true }))
+				{
+					writer.WriteStartObject();
+					writer.WriteStartArray("SubConfigFiles");
+					writer.WriteStringValue(subConfigFilename);
+					writer.WriteEndArray();
+					writer.WriteEndObject();
+				}
+				fs.Flush();
+			}
+
+			// 메인 컨피규레이션 파일을 리로드 할 동안 잠시 대기
+			Thread.Sleep(500);
+
+			// 서브 컨피규레이션 파일을 컨피규레이션 빌더에 넣고 리빌드
+			builder.AddJsonFile(subConfigFilename);
+			config = builder.Build();
+
+			// 메인 컨피규레이션 파일: 서브 컨피규레이션 파일이 잘 추가됐는지 확인
+			Assert.Equal(subConfigFilename, config.GetSection("SubConfigFiles")?.Get<string[]>()?[0]);
+			// 서브 컨피규레이션 파일: 설정항목이 잘 들어갔는지 확인
+			Assert.Equal("true", config["Packet,1,1,1:enabled"]);
 		}
 	}
 }
