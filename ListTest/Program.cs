7b3a6b13e8aacab1641026ecd357eb6a0e123a04
pyramidmine <pyramidmine@gmail.com>
2021-01-30 17:35:00 +0900

    List 테스트
    - List에 객체 추가 후 변경
    - 메서드 파라미터 타입 테스트

---
+++ ListTest/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListTest
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			//
+			// 리스트 A를 B에 Add 한 후 삭제하면 B에 Add 된 리스트는 어떻게 되나?
+			//
+			List<string> source = new List<string>
+			{
+				"Hello",
+				"World"
+			};
+			List<List<string>> target = new List<List<string>>();
+
+			target.Add(source);
+			PrintStringLists(target);
+
+			source.Add("!");
+			PrintStringLists(target);
+
+			source = new List<string>();
+			source.Add("Welcome");
+			PrintStringLists(target);
+
+			AddPage(target, ref source);
+			PrintStringLists(target);
+
+			AddPage(target, ref source);
+			PrintStringLists(target);
+		}
+
+		private static void PrintStringLists(List<List<string>> stringLists)
+		{
+			Console.WriteLine("====== PrintStringLists ======");
+			foreach (var strs in stringLists)
+			{
+				foreach (var str in strs)
+				{
+					Console.WriteLine(str);
+				}
+			}
+		}
+
+		/// <summary>
+		/// 파라미터로 넣어준 객체의 값을 바꿔주는 테스트
+		/// </summary>
+		/// <param name="pages"></param>
+		/// <param name="records"></param>
+		/// <remarks>
+		/// - 일반 파라미터의 경우: records 값을 리셋해도 파라미터 변수 records가 리셋되는 거지 원본 객체가 리셋되는 게 아님
+		/// - 레퍼런스 파라미터의 경우: records 파라미터가 원본을 가리키므로 records에 새 값을 할당하는 순간 원본도 새 값을 가리키게 됨
+		/// </remarks>
+		private static void AddPage(List<List<string>> pages, ref List<string> records)
+		{
+			pages.Add(records);
+			records = new List<string>();
+		}
+	}
+}
