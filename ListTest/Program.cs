77bd204fbf93831a0ff162441ac9554dd6c61d2d
pyramidmine <pyramidmine@gmail.com>
2021-02-18 11:42:20 +0900

    리스트 객체 참조 테스트

--- ListTest/Program.cs
+++ ListTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace ListTest
 {
@@ -7,6 +8,65 @@ namespace ListTest
 	{
 		private static void Main()
 		{
+			TestStringListReference(false);
+			TestListReference(false);
+		}
+
+		/// <summary>
+		/// List 객체 참조 테스트
+		/// </summary>
+		/// <remarks>
+		/// - 객체를 참조하는 경우, 원본 객체가 변경되면 참조하는 쪽에도 변경사항이 반영 됨
+		/// - 원본 객체가 새로 할당되는 경우, 참조 객체는 원본 객체의 기존 값을 유지하므로 연결 관계는 끊어짐
+		/// </remarks>
+		/// <param name="enabled"></param>
+		private static void TestListReference(bool enabled = true)
+		{
+			if (!enabled)
+			{
+				return;
+			}
+
+			// 원본 데이터
+			byte[] data = Encoding.UTF8.GetBytes("12345");
+
+			// 원본 데이터를 포함하는 패킷바디
+			MockPacketBody orgPacketBody = new MockPacketBody { CompressionType = 0, Data = data };
+			MockPacket orgPacket = new MockPacket { PacketType = 1, Bodies = new List<MockPacketBody> { orgPacketBody } };
+
+			// 패킷 데이터를 그대로 참조하는 패킷바디 복사본
+			MockPacket newPacket = new MockPacket { PacketType = 2, Bodies = orgPacket.Bodies };
+
+			// 원본 패킷의 패킷바디 변경
+			orgPacket.Bodies[0].Data = Encoding.UTF8.GetBytes("ABCDE");
+
+			// 원본 패킷의 패킷바디 출력
+			Console.WriteLine($"Original Packet, data:{Encoding.UTF8.GetString(orgPacket.Bodies[0].Data)}");
+			// 클론 패킷의 패킷바디 출력
+			Console.WriteLine($"Clone Packet, data:{Encoding.UTF8.GetString(newPacket.Bodies[0].Data)}");
+
+			// 원본 패킷을 삭제
+			orgPacket = null;
+
+			// 클론 패킷의 패킷바디 출력
+			Console.WriteLine($"Clone Packet, data:{Encoding.UTF8.GetString(newPacket.Bodies[0].Data)}");
+
+			// 원본 패킷을 새로 할당
+			orgPacket = new MockPacket { PacketType = 2, Bodies = new List<MockPacketBody> { new MockPacketBody { CompressionType = 0, Data = Encoding.UTF8.GetBytes("98765") } } };
+
+			// 원본 패킷의 패킷바디 출력
+			Console.WriteLine($"Original Packet, data:{Encoding.UTF8.GetString(orgPacket.Bodies[0].Data)}");
+			// 클론 패킷의 패킷바디 출력
+			Console.WriteLine($"Clone Packet, data:{Encoding.UTF8.GetString(newPacket.Bodies[0].Data)}");
+		}
+
+		private static void TestStringListReference(bool enabled = true)
+		{
+			if (!enabled)
+			{
+				return;
+			}
+
 			//
 			// 리스트 A를 B에 Add 한 후 삭제하면 B에 Add 된 리스트는 어떻게 되나?
 			//
@@ -60,5 +120,17 @@ namespace ListTest
 			pages.Add(records);
 			records = new List<string>();
 		}
+	}
+
+	internal class MockPacket
+	{
+		public int PacketType { get; set; }
+		public List<MockPacketBody> Bodies { get; set; }
+	}
+
+	internal class MockPacketBody
+	{
+		public int CompressionType { get; set; }
+		public byte[] Data { get; set; }
 	}
 }
