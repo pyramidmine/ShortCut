2c0107816a7b1cca55c430b76001a3489cfce31a
MoonHwan Lee <pyramidmine@gmail.com>
2023-06-18 17:52:02 +0900

    String.Join 메서드가 null 멤버 처리하는 방식 테스트

---
+++ StringJoinTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+namespace StringJoinTest;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+		Doc doc = new();
+		doc.desc = "New Documents";
+		Console.WriteLine(doc.ToString());
+	}
+}
+
+internal class Doc
+{
+	public long id;
+	public string name = null;
+	public string desc;
+
+	public override string ToString()
+    {
+		StringBuilder sb = new(1024);
+		sb.AppendJoin('│',
+			id,
+			name,
+            desc);
+		return sb.ToString();
+	}
+}
