31baf6ec7d815ed0bb09903f28a397a71e68e75f
MoonHwan Lee <pyramidmine@gmail.com>
2023-06-20 14:11:53 +0900

    클래스 팩토리 키 타입을 { string, string } 형태로 변경해서 테스트

--- StaticConstructorTest/Program.cs
+++ StaticConstructorTest/Program.cs
@@ -5,16 +5,12 @@ namespace StaticConstructorTest
 {
 	// 제네릭 타입 파라미터를 매 번 지정하기 번거로우면
 	// using 구문을 사용해서 간단하게 사용할 수 있음
-	using PacketFactory = PacketFactory<PacketKeys>;
+	using PacketFactory = PacketFactory<PacketKey>;
 
-	/// <summary>
-	/// 패킷 키를 enum으로 정의
-	/// </summary>
-	internal enum PacketKeys
+	internal struct PacketKey
 	{
-		LoginPacket = 1,
-		LoginResponsePacket,
-		LogoutPacket,
+		public string CallType { get; set; }
+		public string ReqNum { get; set; }
 	}
 
 	internal static class Program
@@ -23,25 +19,31 @@ namespace StaticConstructorTest
 		{
 			Console.WriteLine("Hello World!");
 
-			// 존재하는 패킷 생성 테스트
-			TestCreatePacket(PacketKeys.LoginPacket);
+			// A1Today
+			TestCreatePacket(new PacketKey{ CallType = "CPO1", ReqNum = "MP01_01_V01" });
 
-			// 존재하지 않는 패킷 생성 테스트
-			TestCreatePacket(PacketKeys.LoginResponsePacket);
+			// A2Today
+			TestCreatePacket(new PacketKey{ CallType = "CPO1", ReqNum = "MP02_01_V02" });
+
+			// Unknown
+			TestCreatePacket(new PacketKey{ CallType = "CPO1", ReqNum = "MP03_01_V01" });
 		}
 
-		private static void TestCreatePacket(PacketKeys packetKey)
+		private static void TestCreatePacket(PacketKey key)
 		{
-			Console.WriteLine($"Program.TestCreatePacket: packetKey={packetKey}");
+			Console.WriteLine($"Program.TestCreatePacket: key={key}");
 
-			var packet = PacketFactory.CreatePacket(packetKey);
+			var packet = PacketFactory.CreatePacket(key);
 			if (packet != null)
 			{
-				Console.WriteLine($"Packet created: key={packetKey}, description={packet.Description}");
+				Console.WriteLine($"Packet created: key={key}, description={packet.Description}");
 			}
 			else
 			{
-				Console.WriteLine($"Can't find packet key: key={packetKey}");
+				Console.WriteLine($"Can't find Packet key: key={key}");
+
+				packet = new OraclePacket();
+				Console.WriteLine($"Create default packet: description={packet.Description}");
 			}
 		}
 	}
@@ -58,62 +60,55 @@ namespace StaticConstructorTest
 		abstract public string Description { get; }
 	}
 
-	/// <summary>
-	/// 로그인 패킷 클래스
-	/// </summary>
-	internal class LoginPacket : Packet
+	internal class A1TodayPacket : Packet
 	{
 		/// <summary>
 		/// 패킷팩토리 클래스에서 이 클래스의 생성자를 호출해서 등록을 시도할 예정
 		/// 그런데 패킷팩토리 클래스에서 호출할 때는 객체가 생성되지 않은 상태이므로 일반 생성자는 호출할 수 없음
 		/// 반면, static 생성자는 객체를 생성하지 않은 상태에서도 호출 가능하므로 이 기능을 이용
 		/// </summary>
-		static LoginPacket()
+		static A1TodayPacket()
 		{
-			Console.WriteLine("LoginPacket.sctor");
-			PacketFactory.RegisterPacket(PacketKeys.LoginPacket, typeof(LoginPacket));
+			Console.WriteLine("A1TodayPacket.sctor");
+			PacketFactory.RegisterPacket(new PacketKey { CallType = "CPO1", ReqNum = "MP01_01_V01" }, typeof(A1TodayPacket));
 		}
 
-		public LoginPacket()
+		public A1TodayPacket()
 		{
-			Console.WriteLine("LoginPacket.ctor");
+			Console.WriteLine("A1TodayPacket.ctor");
 		}
 
-		override public string Description { get { return "Login Packet"; } }
+		override public string Description { get { return "A1Today Packet"; } }
 	}
 
-	internal class LogoutPacket : Packet
+	internal class A2TodayPacket : Packet
 	{
-		static LogoutPacket()
+		static A2TodayPacket()
 		{
-			Console.WriteLine("LogoutPacket.sctor");
-			PacketFactory.RegisterPacket(PacketKeys.LoginPacket, typeof(LogoutPacket));
+			Console.WriteLine("A2TodayPacket.sctor");
+			PacketFactory.RegisterPacket(new PacketKey{CallType = "CPO1", ReqNum = "MP02_01_V02"}, typeof(A2TodayPacket));
 		}
 
-		public LogoutPacket()
+		public A2TodayPacket()
 		{
-			Console.WriteLine("LogoutPacket.ctor");
+			Console.WriteLine("A2TodayPacket.ctor");
 		}
 
-		public override string Description => "Logout Packet";
+		public override string Description => "A2Today Packet";
 	}
 
-	/// <summary>
-	/// 패킷 키 값을 기반으로 패킷을 생성해 주는 팩토리 클래스
-	/// </summary>
-	/// <typeparam name="K">패킷을 구분하는 키 타입</typeparam>
+	internal class OraclePacket: Packet
+	{
+		public override string Description => "Oracle Packet";
+	}
+
 	internal static class PacketFactory<K>
 	{
-		/// <summary>
-		/// - 패킷은 키 값으로 구분하며 키 타입은 제네릭 타입을 받아서 처리
-		/// - 키 값에 해당하는 패킷 타입이 저장됨
-		/// - 사용할 때는 특정 키 값을 주면 XyzPacket 객체를 생성하는 식
-		/// </summary>
 		private static readonly Dictionary<K, Type> packets = new Dictionary<K, Type>();
 
 		static PacketFactory()
 		{
-			Console.WriteLine("PacketFactory.PacketFactory");
+			Console.WriteLine("static PacketHandlerFactory()");
 			var baseType = typeof(Packet);
 
 			// 등록할 패킷 타입을 찾기 위해 모든 어셈블리 검색
@@ -126,10 +121,10 @@ namespace StaticConstructorTest
 					// 패킷 타입을 상속 받았거나 또는 인터페이스를 포함하는 경우만 등록 허용
 					if (IsSubclassOfOrHasInterface(type, baseType))
 					{
-						Console.WriteLine($"PacketFactory.PacketFactory: {type.Name} class is registered.");
 						// 등록하려는 객체의 static 생성자를 호출
 						// 해당 객체의 static 생성자는 패킷팩토리 메서드를 호출해서 각자 등록과정 수행
 						System.Runtime.CompilerServices.RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+						Console.WriteLine($"PacketFactory.PacketFactory: {type.Name} class is registered.");
 					}
 				}
 			}
@@ -169,17 +164,14 @@ namespace StaticConstructorTest
 			Console.WriteLine("PacketFactory.RegisterPacket");
 			try
 			{
-				// 키가 중복되는 경우를 보여주기 위해 키 존재 체크를 하지 않았음
-				packets.Add(key, type);
-
-				// 실제 릴리즈 할 때는 익셉션 발생을 피하기 위해 키가 존재하는 지 체크하는 게 좋음
-				// 개발 단계에서 키 중복 체크는 유닛테스트 활용
-				/*
-				if (!packets.ContainsKey(key))
+				#if RELEASE
+				if (packets.ContainsKey(key))
 				{
-					packets.Add(key, type);
+					return;
 				}
-				*/
+				#endif
+
+				packets.Add(key, type);
 			}
 			catch (Exception ex)
 			{
